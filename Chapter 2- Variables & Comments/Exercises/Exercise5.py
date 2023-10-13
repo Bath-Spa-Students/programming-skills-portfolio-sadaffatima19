6673546115e2427e41a599443835d62ac5954cdb
Sadaf Fatima <fatimasadaf68@gmail.com>
2023-10-13 19:21:04 +0400

    Exercise5.py

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise5.py
@@ -0,0 +1,9 @@
+price = 6
+total_money = 50
+
+num = total_money // price
+
+money_left = total_money % price
+
+print(f"Number of USB sticks she can buy: {num}")
+print(f"Money left after buying USB sticks: {money_left}")
