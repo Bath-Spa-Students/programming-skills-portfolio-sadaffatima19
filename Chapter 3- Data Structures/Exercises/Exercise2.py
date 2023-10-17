8873caa71e66bfbb1e2b7400ad22eca5666c0158
Sadaf Fatima <fatimasadaf68@gmail.com>
2023-10-17 20:17:32 +0400

    Exercise2.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise2.py
@@ -0,0 +1,6 @@
+names = ["Feba", "Rahma", "Alia", "Angel", "Ammara"]
+print(f"{names[0]} is my friend.")
+print(f"{names[1]} is my friend.")
+print(f"{names[2]} is my friend.")
+print(f"{names[3]} is my friend.")
+print(f"{names[4]} is my friend.")
