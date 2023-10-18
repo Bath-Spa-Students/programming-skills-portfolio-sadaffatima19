efddc50fde7f34cd8e77661c75d97a7fc1d74f2b
Sadaf Fatima <fatimasadaf68@gmail.com>
2023-10-18 15:14:56 +0400

    Exercise4.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise4.py
@@ -0,0 +1,4 @@
+members = ["Rahma", "Alia", "Ammara"]
+print(f"Since you're always hungry, I want to invite you for dinner. Please come {members[0]}.")
+print(f"I made your favorite meal for dinner. Please come {members[1]}.")
+print(f"If you want to have the most delicious dinner tonight. Please come {members[1]}.")
