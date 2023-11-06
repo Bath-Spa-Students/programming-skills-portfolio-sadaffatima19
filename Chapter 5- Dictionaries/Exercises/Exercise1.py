42f6b472e6dafe9d856d969700024dae546e98bd
Sadaf Fatima <144330231+sadaffatima19@users.noreply.github.com>
2023-11-06 20:54:39 +0400

    Exercise1.py

---
+++ Chapter 5- Dictionaries/Exercises/Exercise1.py
@@ -0,0 +1,8 @@
+'''Use a dictionary to store information about a person you know. Store their first name, last name, age, and the city in which they live.
+You should have keys such as first_name, last_name, age, and city. Print each piece of information stored in your dictionary.'''
+
+friend = {'first_name': 'Rahma', 'last_name': 'Reyaz', 'age': 18, 'city': 'Sharjah'}
+print(friend['first_name'])
+print(friend['last_name'])
+print(friend['age'])
+print(friend['city'])
