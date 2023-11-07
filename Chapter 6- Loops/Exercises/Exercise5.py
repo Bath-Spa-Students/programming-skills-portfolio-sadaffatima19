aab313f2197921b4c6bb334de8e1baf6028e7d2a
Sadaf Fatima <144330231+sadaffatima19@users.noreply.github.com>
2023-11-07 12:03:38 +0400

    Exercise5.py

---
+++ Chapter 6- Loops/Exercises/Exercise5.py
@@ -0,0 +1,20 @@
+'''Using the list sandwich_orders from Exercise 7-8, make sure the sandwich 'pastrami' appears in the list at least three times. Add code near the beginning of your program to 
+print a message saying the deli has run out of pastrami, and then use a while loop to remove all occurrences of 'pastrami' from sandwich_orders. 
+Make sure no pastrami sandwiches end up in finished_sandwiches.'''
+
+sandwich_orders = ["Egg Sandwich", "Chicken Sandwich", "Cheese Sandwich", "Club Sandwich"]
+finished_sandwiches = []
+
+print("I'm sorry, we're out of Chicken Sandwich.")
+while 'Chicken Sandwich' in sandwich_orders:
+    sandwich_orders.remove('Chicken Sandwich')
+
+print("\n")
+while sandwich_orders:
+    new_sandwich = sandwich_orders.pop()
+    print("I'm preparing your " + new_sandwich)
+    finished_sandwiches.append(new_sandwich)
+
+print("\n")
+for sandwich in finished_sandwiches:
+    print("I made a " + sandwich)
