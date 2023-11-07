f1da79a3efc969ca8622d97a4c3f3be61ad197ea
Sadaf Fatima <144330231+sadaffatima19@users.noreply.github.com>
2023-11-07 12:01:51 +0400

    Exercise4.py

---
+++ Chapter 6- Loops/Exercises/Exercise4.py
@@ -0,0 +1,16 @@
+'''Make a list called sandwich_orders and fill it with the names of various sandwiches. Then make an empty list called finished_sandwiches.
+Loop through the list of sandwich orders and print a message for each order, such as I made your tuna sandwich. As each sandwich is made,
+move it to the list of finished sandwiches. After all the sandwiches have been made, print a message listing each sandwich that was made.'''
+
+sandwich_orders = ["Egg Sandwich", "Chicken Sandwich", "Cheese Sandwich", "Club Sandwich"]
+finished_sandwiches = []
+
+print("\n")
+while sandwich_orders:
+    new_sandwich = sandwich_orders.pop()
+    print("I'm preparing your " + new_sandwich)
+    finished_sandwiches.append(new_sandwich)
+
+print("\n")
+for sandwich in finished_sandwiches:
+    print("I made a " + sandwich)
