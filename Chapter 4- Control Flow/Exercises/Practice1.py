25387846276f139605564d653ebfc92812cf356b
Sadaf Fatima <144330231+sadaffatima19@users.noreply.github.com>
2023-11-12 16:19:05 +0400

    Practice1.py

---
+++ Chapter 4- Control Flow/Exercises/Practice1.py
@@ -0,0 +1,8 @@
+'''Write a python program with if statement that assigns 20 to the variable y and assigns 40 to the variable z if the variable x is greater than 100.'''
+
+x = 150
+if x > 100:
+  y = 20
+  z = 40
+  print("The value of y is", y)
+  print("The value of z is", z)
