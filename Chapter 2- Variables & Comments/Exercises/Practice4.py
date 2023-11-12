a771499a8258572751b638c8d2bcd56e0fb54cc4
Sadaf Fatima <144330231+sadaffatima19@users.noreply.github.com>
2023-11-12 14:48:12 +0400

    Practice4.py

---
+++ Chapter 2- Variables & Comments/Exercises/Practice4.py
@@ -0,0 +1,7 @@
+'''Write a python program that stores an integer and string value to variables x and y. Print the type of
+variable x and y.'''
+
+x = 19
+y = "Sadaf"
+print(type(x))
+print(type(y))
