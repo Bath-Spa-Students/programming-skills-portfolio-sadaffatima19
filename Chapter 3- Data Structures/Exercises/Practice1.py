0baa65fc2b8502f69cf168835ac8f01d5b64201d
Sadaf Fatima <144330231+sadaffatima19@users.noreply.github.com>
2023-11-12 15:55:10 +0400

    Practice1.py

---
+++ Chapter 3- Data Structures/Exercises/Practice1.py
@@ -0,0 +1,8 @@
+'''Given a Python list, write a program to remove all occurrences of item 20.
+Given list: list1 = [5, 20, 15, 20, 25, 50, 20]'''
+
+list1 = [5, 20, 15, 20, 25, 50, 20]
+list1.pop(1)
+list1.pop(2)
+list1.pop(4)
+print(list1)
