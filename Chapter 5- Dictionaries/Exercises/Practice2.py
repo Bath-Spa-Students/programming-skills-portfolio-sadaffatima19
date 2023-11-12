132abdc5599d391bdf7ddb3109fb977f8f17f624
Sadaf Fatima <144330231+sadaffatima19@users.noreply.github.com>
2023-11-12 18:40:39 +0400

    Practice2.py

---
+++ Chapter 5- Dictionaries/Exercises/Practice2.py
@@ -0,0 +1,7 @@
+'''Write a Python program to iterate through the keys of a dictionary and print them.'''
+
+myself = {"Name": "Sadaf Fatima", "Age": 17, "Weight": "53kg", "Hobby": "Reading"}
+print(myself)
+
+for key in myself:
+  print(key)
