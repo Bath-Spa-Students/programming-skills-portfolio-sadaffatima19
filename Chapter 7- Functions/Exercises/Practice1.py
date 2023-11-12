acefe57c61ae0dfc97c6f75c77d99eeb762cc91f
Sadaf Fatima <144330231+sadaffatima19@users.noreply.github.com>
2023-11-12 19:25:36 +0400

    Practice1.py

---
+++ Chapter 7- Functions/Exercises/Practice1.py
@@ -0,0 +1,6 @@
+'''Write a Python function that takes two numbers as arguments and returns their sum.'''
+
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+sum = a + b
+print("The sum is:", sum)
